9a544df7fb9cca5038c368e747a88392b51a579a
Mikhail Zolotukhin <mvz@fb.com>
2020-01-24 08:06:53 -0800

    Fix amsjit tests. We needed to explicitly list asmjit in test_tensorexpr target link libraries

--- test/cpp/tensorexpr/test_asmjit.cpp
+++ test/cpp/tensorexpr/test_asmjit.cpp
@@ -8,62 +8,47 @@ namespace jit {
 
 using namespace torch::jit::compiler;
 
-// XXX: ASMJit codegen is broken due to some linker error, disabling the tests
-// until it's fixed.
-
-// #define ASMJIT_TESTS_ENABLED
-
 void testAsmjitIntImmTest() {
-#ifdef ASMJIT_TESTS_ENABLED
   auto a = IntImm::make(2);
   ASMJITCodeGen cg;
   a.accept(&cg);
   EXPECT_EQ(cg.value(), 2);
-#endif
 }
 
 void testAsmjitIntAddTest() {
-#ifdef ASMJIT_TESTS_ENABLED
   auto a = IntImm::make(2);
   auto b = IntImm::make(3);
   auto c = Add::make(a, b);
   ASMJITCodeGen cg;
   c.accept(&cg);
   EXPECT_EQ(cg.value(), 5);
-#endif
 }
 
 void testAsmjitIntSubTest() {
-#ifdef ASMJIT_TESTS_ENABLED
   auto a = IntImm::make(2);
   auto b = IntImm::make(3);
   auto c = Sub::make(a, b);
   ASMJITCodeGen cg;
   c.accept(&cg);
   EXPECT_EQ(cg.value(), -1);
-#endif
 }
 
 void testAsmjitIntMulTest() {
-#ifdef ASMJIT_TESTS_ENABLED
   auto a = IntImm::make(2);
   auto b = IntImm::make(3);
   auto c = Mul::make(a, b);
   ASMJITCodeGen cg;
   c.accept(&cg);
   EXPECT_EQ(cg.value(), 6);
-#endif
 }
 
 void testAsmjitIntDivTest() {
-#ifdef ASMJIT_TESTS_ENABLED
   auto a = IntImm::make(6);
   auto b = IntImm::make(3);
   auto c = Div::make(a, b);
   ASMJITCodeGen cg;
   c.accept(&cg);
   EXPECT_EQ(cg.value(), 2);
-#endif
 }
 
 } // namespace jit
