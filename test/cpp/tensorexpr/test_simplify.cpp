9a3a6ccc38a7ddd91757f5b1d257911b0c65bfbb
nickgg <nickg@fb.com>
2020-03-12 16:59:56 +1100

    Add a hasher for checking equality of sub-exprs

--- test/cpp/tensorexpr/test_simplify.cpp
+++ test/cpp/tensorexpr/test_simplify.cpp
@@ -2,6 +2,7 @@
 
 #include "test/cpp/tensorexpr/test_utils.h"
 #include "torch/csrc/jit/tensorexpr/constant_folder.h"
+#include "torch/csrc/jit/tensorexpr/hash_server.h"
 
 namespace torch {
 namespace jit {
@@ -53,7 +54,7 @@ void testConstantFoldShifts() {
   EXPECT_EQ(newF.AsNode<IntImm>()->value(), 14);
 
   SimpleIRExprEval eval(newF);
-  EXPECT_EQ(eval.value<int>(), 7 << (4-3));
+  EXPECT_EQ(eval.value<int>(), 7 << (4 - 3));
 }
 
 void testConstantFoldBitwise() {
@@ -168,6 +169,187 @@ void testUnFoldableExpr() {
   result = Let::make(y, ExprHandle(2.f), result);
   SimpleIRExprEval eval(result);
   EXPECT_EQ(eval.value<float>(), 9 + 10);
+}
+
+void testHashSimple() {
+  KernelScope kernel_scope;
+  VarHandle x("x", kFloat);
+  ExprHandle a(2.0f);
+  ExprHandle b(3.0f);
+  ExprHandle f = a + b * x;
+
+  HashProvider hasher;
+
+  auto hash_x = hasher.hash(x.node());
+  auto hash_a = hasher.hash(a.node());
+  auto hash_f = hasher.hash(f.node());
+
+  EXPECT_NE(hash_x, 0);
+  EXPECT_NE(hash_a, 0);
+  EXPECT_NE(hash_f, 0);
+  EXPECT_NE(hash_x, hash_a);
+  EXPECT_NE(hash_x, hash_f);
+  EXPECT_NE(hash_a, hash_f);
+}
+
+void testHashEquivalence() {
+  KernelScope kernel_scope;
+  VarHandle x("x", kFloat);
+  VarHandle y("y", kFloat);
+  ExprHandle f = (x * y) + (x * y);
+
+  const Add* root = f.AsNode<Add>();
+  EXPECT_NE(root, nullptr);
+
+  HashProvider hasher;
+  auto hash_f = hasher.hash(f.node());
+  auto hash_l = hasher.hash(root->lhs());
+  auto hash_r = hasher.hash(root->rhs());
+
+  // Root not equal to either branch.
+  EXPECT_NE(hash_f, hash_l);
+  EXPECT_NE(hash_f, hash_r);
+  // but branches are equal.
+  EXPECT_EQ(hash_l, hash_r);
+
+  // Still equivalent if separate.
+  ExprHandle a(2);
+  ExprHandle f2 = x + a / y;
+  ExprHandle b(2);
+  ExprHandle f3 = x + b / y;
+  EXPECT_EQ(hasher.hash(f2.node()), hasher.hash(f3.node()));
+
+  // Not equivalent if different vars (even with same name).
+  VarHandle z("x", kFloat);
+  ExprHandle f4 = z + b / y;
+  EXPECT_NE(hasher.hash(f2.node()), hasher.hash(f4.node()));
+
+  // Intrinsics sanity check.
+  ExprHandle f5 = Intrinsics::make(kSin, x) * Intrinsics::make(kCos, x);
+  EXPECT_NE(hasher.hash(f5.node()), 0);
+}
+
+void testHashEquivalenceAfterFolding() {
+  KernelScope kernel_scope;
+  VarHandle x("x", kFloat);
+  ExprHandle a(2.0f);
+  ExprHandle b(3.0f);
+  ExprHandle c(5.0f);
+  ExprHandle f = ((a + b) * x) * (c * x);
+
+  const Mul* root = f.AsNode<Mul>();
+  EXPECT_NE(root, nullptr);
+
+  HashProvider hasher;
+  auto hash_f = hasher.hash(f.node());
+  auto hash_l = hasher.hash(root->lhs());
+  auto hash_r = hasher.hash(root->rhs());
+
+  // Root not equal to either branch, and branches not equal.
+  EXPECT_NE(hash_f, hash_l);
+  EXPECT_NE(hash_f, hash_r);
+  EXPECT_NE(hash_l, hash_r);
+
+  ConstantFolder folder;
+  ExprHandle newF = ExprHandle(f.node()->accept_mutator(&folder));
+
+  const Mul* newRoot = newF.AsNode<Mul>();
+  EXPECT_NE(newRoot, nullptr);
+
+  auto hash_f_n = hasher.hash(newF.node());
+  auto hash_l_n = hasher.hash(newRoot->lhs());
+  auto hash_r_n = hasher.hash(newRoot->rhs());
+
+  // Root not equal to either branch.
+  EXPECT_NE(hash_f_n, hash_l_n);
+  EXPECT_NE(hash_f_n, hash_r_n);
+  // but branches are now equal.
+  EXPECT_EQ(hash_l_n, hash_r_n);
+}
+
+void testHashDifferenceTypes() {
+  KernelScope kernel_scope;
+
+  HashProvider hasher;
+  std::vector<const Expr*> immediates;
+
+  immediates.push_back(new DoubleImm(1));
+  immediates.push_back(new FloatImm(1));
+  immediates.push_back(new HalfImm(1));
+  immediates.push_back(new BoolImm(1));
+  immediates.push_back(new CharImm(1));
+  immediates.push_back(new ByteImm(1));
+  immediates.push_back(new ShortImm(1));
+  immediates.push_back(new IntImm(1));
+  immediates.push_back(new LongImm(1));
+
+  // Immediates of different types are not equal.
+  for (unsigned int i = 0; i < immediates.size(); ++i) {
+    for (unsigned int j = i + 1; j < immediates.size(); ++j) {
+      EXPECT_NE(hasher.hash(immediates[i]), hasher.hash(immediates[j]));
+    }
+  }
+
+  // But coerced immediates are if they are the same type:
+  ConstantFolder folder;
+  ExprHandle f1 = ExprHandle(2.f) + CharImm::make(1);
+  ExprHandle f2 = Cast::make(kFloat, IntImm::make(3));
+
+  ExprHandle ff1 = ExprHandle(f1.node()->accept_mutator(&folder));
+  ExprHandle ff2 = ExprHandle(f2.node()->accept_mutator(&folder));
+
+  EXPECT_EQ(hasher.hash(ff1.node()), hasher.hash(ff2.node()));
+}
+
+void testHashLargeExpression() {
+  KernelScope kernel_scope;
+  constexpr int N = 1024;
+  Buffer a(VarHandle("A", kHandle), kInt, {N});
+  Buffer b(VarHandle("B", kHandle), kInt, {N});
+  Buffer c(VarHandle("C", kHandle), kInt, {N});
+  auto mask = IntImm::make(1);
+  VarHandle i("i", kInt);
+  auto memcpy_stmt = For::make(
+      i,
+      0,
+      N,
+      Store::make(
+          c,
+          i,
+          CompareSelect::make(
+              Load::make(a, i, mask),
+              Load::make(b, i, mask),
+              CompareSelectOperation::kEQ),
+          mask));
+
+  Buffer d(VarHandle("D", kHandle), kInt, {1});
+  Buffer e(VarHandle("E", kHandle), kInt, {1});
+  auto store_ramp_stmt = Store::make(
+      e,
+      Ramp::make(0, 1, 4),
+      Load::make(d, Ramp::make(0, 1, 4), Broadcast::make(IntImm::make(1), 4)),
+      Broadcast::make(Cast::make(kInt, DoubleImm::make(1)), 4));
+
+  auto if_stmt = Cond::make(
+      CompareSelect::make(
+          Load::make(a, i, mask),
+          Load::make(b, i, mask),
+          CompareSelectOperation::kGE),
+      memcpy_stmt,
+      store_ramp_stmt);
+
+  HashProvider hasher;
+  auto hash_r = hasher.hash(if_stmt);
+  // We should not have to do any more work.
+  EXPECT_TRUE(hasher.cachedHash(memcpy_stmt));
+  auto hash_t = hasher.hash(memcpy_stmt);
+  EXPECT_TRUE(hasher.cachedHash(store_ramp_stmt));
+  auto hash_f = hasher.hash(store_ramp_stmt);
+
+  // Root not equal to either branch, and branches not equal.
+  EXPECT_NE(hash_r, hash_t);
+  EXPECT_NE(hash_r, hash_f);
+  EXPECT_NE(hash_t, hash_f);
 }
 
 } // namespace jit
